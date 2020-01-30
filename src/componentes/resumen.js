4dfdea331caa0bbb4d96d5f8e745ae5bb63069ec
Erica <juan.rompani@gmail.com>
2020-01-29 21:41:56 -0300

    Commit App Seguros

---
+++ src/componentes/resumen.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { directive } from '@babel/types';
+import { primeraMayuscula } from '../helper';
+
+class Resumen extends Component{
+    
+   
+
+
+    mostrarResumen = () => {
+
+        const { marca, year, plan } = this.props.datos;
+
+        if(!marca || !year || !plan) return null;
+        
+        return(
+            <div className="resumen" >
+            <h2>Resumen de la Cotizacion</h2>
+            <p>Marca: { primeraMayuscula( marca ) }</p>
+            <p>Año: { year}</p>
+            <p>Plan: { primeraMayuscula( plan ) }</p>
+        </div>
+        )
+    }
+    
+    
+    render(){
+        
+
+        return(
+            <div>
+                {this.mostrarResumen()}
+            </div>
+        )
+    }
+
+
+
+
+
+
+}
+export default Resumen;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import App from './componentes/app';
 import * as serviceWorker from './serviceWorker';
 
+//const titulo= React.createElement('h1', {id:'titulo'}, 'Hola Mundo');
+//const cuerpo= React.createElement('h2', {id: 'subtitulo'}, "Principios de Programacion")
+
+//ReactDOM.render(titulo, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
